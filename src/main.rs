3ed3966bf316e2a6fb15e56b7805e845d8319a91
longhodac-ucsd <long110404@gmail.com>
2026-02-09 23:21:30 -0800

    learned about for loops and unused variable lint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main(){
-    println!("Hello world!");
+    // the compiler will warn you if you have "i" but don't use it in the loop
+    for _ in 1..6 {
+        println!("Hello world");
+    }
+
+    for i in 1..6 {
+        println!("hello world {i}");
+    }
 }
