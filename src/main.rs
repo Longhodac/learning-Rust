80b59ba21583272344155a8e1af833917d51ed34
longhodac-ucsd <long110404@gmail.com>
2026-02-13 22:51:20 -0800

    tuples

--- src/main.rs
+++ src/main.rs
@@ -23,4 +23,13 @@ fn main(){
         }; // all let binding need this semicolon
     
         println!("{} -> {}", n, big_n);
+
+        let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                      -1i8, -2i16, -3i32, -4i64,
+                      0.1f32, 0.2f64,
+                      'a', true);
+
+    // Values can be extracted from the tuple using tuple indexing.
+    println!("Long tuple first value: {}", long_tuple.0);
+    println!("Long tuple second value: {}", long_tuple.1);
 }
