ed499667aed0cf093200b2abff1273f615827d75
longhodac <long110404@gmail.com>
2026-03-10 23:06:04 -0700

    hi

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,6 @@ fn main(){
     else if n > 0 {
         print!("{} is positive", n);
     }
-    else if n > 0 {
-        print!("{} is positive", n);
-    }
     else {
         print!("{} is zero", n);
     }
@@ -22,18 +19,8 @@ fn main(){
             10 * n
         } else {
             print!(", and is a big number, halve the number");
-            n / 2
+            n / 2.2
         }; // all let binding need this semicolon
     
         println!("{} -> {}", n, big_n);
-
-        let long_tuple = (1u8, 2u16, 3u32, 4u64,
-                      -1i8, -2i16, -3i32, -4i64,
-                      0.1f32, 0.2f64,
-                      'a', true);
-
-    // Values can be extracted from the tuple using tuple indexing.
-    println!("Long tuple first value: {}", long_tuple.0);
-    println!("Long tuple second value: {}", long_tuple.1); println!("Long tuple second value: {}", long_tuple.1);
-    println!("Long tuple second value: {}", long_tuple.1);
 }
